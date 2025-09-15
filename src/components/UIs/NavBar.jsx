da91fefeddfa36077c8edb1213b8951dd9981b22
victor grinan <victorgrinan@gmail.com>
2025-09-15 16:34:25 +0300

    little css improvements

--- src/components/UIs/NavBar.jsx
+++ src/components/UIs/NavBar.jsx
@@ -1,19 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { useSelector } from "react-redux";
+import Form from "react-bootstrap/Form";
+import { useSelector, 
+  // useDispatch 
+} from "react-redux";
+import { Button } from "react-bootstrap";
+// import { setSort } from "../features/countries/countriesSlice";
 
 const NavBar = () => {
-  const favoriteCount = useSelector(state => state.countries.favoriteCountries.length);
+  const favoriteCount = useSelector(
+    (state) => state.countries.favoriteCountries.length
+  );
+  // const dispatch = useDispatch();
+  const location = useLocation();
+
+  const handleSort = (e) => {
+    console.log('sorting by', e.target.value);
+    // dispatch(setSort(e.target.value));
+  };
+
+  const showFilters = location.pathname.endsWith("/browse");
+
   return (
-    <Navbar
-      bg="light"
-      expand="lg"
-      style={{
-        width: "100vw",
-      }}
-    >
+    <Navbar bg="light" expand="lg" sticky="top" style={{ width: "100vw" }}>
       <Container>
         <Navbar.Brand as={Link} to="/">
           Countries App
@@ -31,12 +42,49 @@ const NavBar = () => {
             </Nav.Link>
 
             <Nav.Link as={Link} to="/favorites">
-              Favorites  
-              {favoriteCount > 0 && 
-                 <span style={{backgroundColor:"#ff4d6d", borderRadius: '100px', width:'30px', height:'30px', fontSize:'10px', color:'white', padding:'2px' }}> {favoriteCount} </span>
-              }
+              Favorites{" "}
+              {favoriteCount > 0 && (
+                <span
+                  style={{
+                    backgroundColor: "#ff4d6d",
+                    borderRadius: "100px",
+                    width: "30px",
+                    height: "30px",
+                    fontSize: "10px",
+                    color: "white",
+                    padding: "2px",
+                  }}
+                >
+                  {favoriteCount}
+                </span>
+              )}
             </Nav.Link>
           </Nav>
+
+          {/* ✅ Only show on /browse */}
+          {showFilters && (
+            <>
+              <Form.Select
+                aria-label="Sort countries"
+                className="me-2"
+                onChange={handleSort}
+                style={{ maxWidth: "200px" }}
+              >
+                <option value="name">Sort by Name</option>
+                <option value="population">Sort by Population</option>
+                <option value="currency">Sort by Currency</option>
+              </Form.Select>
+              <Form className="d-flex me-3">
+                <Form.Control
+                  type="search"
+                  placeholder="Search countries"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </>
+          )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
@@ -44,4 +92,3 @@ const NavBar = () => {
 };
 
 export default NavBar;
-
