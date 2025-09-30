e2593ebbf36671e30bc24a1d5edfbf023efc17e3
victor grinan <victorgrinan@gmail.com>
2025-09-30 11:05:01 +0300

    delete home tab

--- src/components/UIs/NavBar.jsx
+++ src/components/UIs/NavBar.jsx
@@ -34,9 +34,6 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">
-              Home
-            </Nav.Link>
 
             <Nav.Link as={Link} to="/browse">
               Browse
